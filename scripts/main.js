54cec362eb19a68f8eb0881795b658d90c50088d
Filip <filip373@gmail.com>
2016-06-20 19:26:29 +0200

    tabs implemented using jQuery without plugin

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,11 @@ $(function() {
     .children('a').css({
       'text-decoration': 'none',
       color: 'inherit'
+    })
+    .click(function(event) {
+      event.preventDefault();
+      $('.container > section').hide();
+      $(this.getAttribute('href')).show();
     });
 
   $('#home').append('<img src="images/photo.jpg">')
@@ -33,9 +38,11 @@ $(function() {
     .append('<p>Integer ultrices volutpat suscipit. Nullam feugiat mi sit amet lorem hendrerit, non accumsan velit dapibus. Fusce mollis justo id leo pulvinar ornare. Maecenas et lorem erat. Fusce non enim blandit ligula placerat faucibus ut eu nunc. Duis aliquet metus a metus pellentesque posuere. Nulla pharetra ipsum velit. Fusce nisi tortor, porttitor id ex quis, semper malesuada tortor. Etiam risus ante, imperdiet eu quam et, euismod euismod erat. Suspendisse vehicula magna vel justo ullamcorper imperdiet. Fusce in magna at erat varius convallis.</p>');
 
   $('#about').append('<h1>About</h1>')
-    .append('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus libero eu orci hendrerit, vel porta felis euismod. Fusce nunc mi, sollicitudin ut rutrum a, tristique quis dolor. Proin pulvinar nibh augue, at interdum augue euismod eu. Sed porttitor euismod leo ut elementum. Cras pharetra ex ut fringilla accumsan. Donec nec lorem eu velit laoreet blandit. Maecenas fringilla sem vestibulum ex consectetur consectetur. Etiam sed nibh consequat, molestie sapien vel, lobortis risus. Pellentesque viverra nisi ac elit maximus, sed tincidunt nibh consectetur.</p>');
+    .append('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus libero eu orci hendrerit, vel porta felis euismod. Fusce nunc mi, sollicitudin ut rutrum a, tristique quis dolor. Proin pulvinar nibh augue, at interdum augue euismod eu. Sed porttitor euismod leo ut elementum. Cras pharetra ex ut fringilla accumsan. Donec nec lorem eu velit laoreet blandit. Maecenas fringilla sem vestibulum ex consectetur consectetur. Etiam sed nibh consequat, molestie sapien vel, lobortis risus. Pellentesque viverra nisi ac elit maximus, sed tincidunt nibh consectetur.</p>')
+    .hide();
 
   $('#contact').append('<h1>Contact</h1>')
-    .append('<p>Integer ultrices volutpat suscipit. Nullam feugiat mi sit amet lorem hendrerit, non accumsan velit dapibus. Fusce mollis justo id leo pulvinar ornare. Maecenas et lorem erat. Fusce non enim blandit ligula placerat faucibus ut eu nunc. Duis aliquet metus a metus pellentesque posuere. Nulla pharetra ipsum velit. Fusce nisi tortor, porttitor id ex quis, semper malesuada tortor. Etiam risus ante, imperdiet eu quam et, euismod euismod erat. Suspendisse vehicula magna vel justo ullamcorper imperdiet. Fusce in magna at erat varius convallis.</p>');
+    .append('<p>Integer ultrices volutpat suscipit. Nullam feugiat mi sit amet lorem hendrerit, non accumsan velit dapibus. Fusce mollis justo id leo pulvinar ornare. Maecenas et lorem erat. Fusce non enim blandit ligula placerat faucibus ut eu nunc. Duis aliquet metus a metus pellentesque posuere. Nulla pharetra ipsum velit. Fusce nisi tortor, porttitor id ex quis, semper malesuada tortor. Etiam risus ante, imperdiet eu quam et, euismod euismod erat. Suspendisse vehicula magna vel justo ullamcorper imperdiet. Fusce in magna at erat varius convallis.</p>')
+    .hide();
 
 });
